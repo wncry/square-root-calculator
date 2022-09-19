d8909bd98b9f019cbea6f001c6ecb1a73dff83dd
Jonathan <78560204+wncry@users.noreply.github.com>
2022-09-19 08:19:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 import cmath
 import math
-x = float(input("""What number would you like to square root, 
-""" ))   
-
+x = float(input("Input number to find its square root: "))  # you can use \n if you want to add a linebreak
 
 def sqrt(x):
-      
-  
-
-  
   if x < 0:
     crt = cmath.sqrt(x)
-    print(crt, """
-
-('j' is used instead of 'i')""")
-  if x > 0 or x == 0 :
+    print(crt)
+  elif x > 0 or x == 0 :  # i think x >= 0 does both
     rt = math.sqrt(x)
     print(rt)
-  
-  
-  
 
 sqrt(x)
-
-  
