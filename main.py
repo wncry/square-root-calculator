2fa0ec072e949f3c38b6c1e895799ea9af6b03ce
Simply-Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2022-09-19 16:32:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import cmath
 import math
-x = float(input("""What number would you like to square root 
+print("Note: For complex numbers the letter 'j' is used instead of 'i'")
+x = float(input("""What number would you like to square root, 
 """ ))   
+
+
 def sqrt(x):
       
-  rt = math.sqrt(x)
-  print(rt)
+  
+
+  
+  if x < 0:
+    crt = cmath.sqrt(x)
+    print(crt)
+  if x > 0 or x == 0 :
+    rt = math.sqrt(x)
+    print(rt)
+  
+  
   
 
 sqrt(x)
+
   
